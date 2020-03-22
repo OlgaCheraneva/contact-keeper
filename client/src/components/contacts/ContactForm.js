8f00454d115eb453a192b1a261e53757ea30a9d0
Olga <cheraneva197@gmail.com>
2020-03-22 17:25:40 +0500

    Setting contact form as a functional component using react useState hook

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+
+export const ContactForm = () => {
+    const [contact, setContact] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        type: 'personal'
+    });
+
+    const {name, email, phone, type} = contact;
+
+    const onChange = (e) =>
+        setContact({...contact, [e.target.name]: e.target.value});
+
+    return (
+        <form>
+            <h2 className="text-primary">Add Contact</h2>
+            <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={onChange}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={onChange}
+            />
+            <input
+                type="text"
+                placeholder="Phone"
+                name="phone"
+                value={phone}
+                onChange={onChange}
+            />
+            <h5>Contact Type</h5>
+            <label>
+                <input
+                    type="radio"
+                    name="type"
+                    value="personal"
+                    checked={type === 'personal'}
+                    onChange={onChange}
+                />{' '}
+                Personal{' '}
+            </label>
+            <label>
+                <input
+                    type="radio"
+                    name="type"
+                    value="professional"
+                    checked={type === 'professional'}
+                    onChange={onChange}
+                />{' '}
+                Professional
+            </label>
+            <div>
+                <input
+                    type="submit"
+                    value="Add Contact"
+                    className="btn btn-primary btn-block"
+                />
+            </div>
+        </form>
+    );
+};
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import {Contacts} from '../contacts/Contacts';
+import {ContactForm} from '../contacts/ContactForm';
 
 export const Home = () => {
     return (
         <div className="grid-2">
+            <div>
+                <ContactForm />
+            </div>
             <div>
                 <Contacts />
             </div>
