e0a63bed5a74099f766b985069b76d6dd1e5869b
Olga <cheraneva197@gmail.com>
2020-03-22 17:23:26 +0500

    Setting contacts context and global state

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 export const Navbar = ({title, icon}) => {
     return (
@@ -7,11 +8,19 @@ export const Navbar = ({title, icon}) => {
             <h1>
                 <i className={icon} /> {title}
             </h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
         </div>
     );
 };
 
-Navbar.PropTypes = {
+Navbar.propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string
 };
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,6 +7,9 @@ export const About = () => {
             <p className="my-1">
                 This is a full stack React app for keeping contacts
             </p>
+            <p className="bg-dark p">
+                <strong>Version: </strong>1.0.0
+            </p>
         </div>
     );
 };
