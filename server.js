2acb36439e6462e24b03b4cb93d858e28513c853
Olga <cheraneva197@gmail.com>
2020-04-05 16:01:21 +0500

    Serving static assets in production

--- server.js
+++ server.js
@@ -16,5 +16,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (_req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
